8d3612638495a6f4a4600be3c0c31fabe0485e21
oldwei <oldweipro@gmail.com>
2021-05-31 12:09:03 +0800

    初始化项目

---
+++ src/main/java/com/ultronvision/fast/FastBootApplication.java
@@ -0,0 +1,15 @@
+package com.ultronvision.fast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author oldwei
+ * @date 2021/5/31 11:23 上午
+ */
+@SpringBootApplication
+public class FastBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FastBootApplication.class, args);
+    }
+}
